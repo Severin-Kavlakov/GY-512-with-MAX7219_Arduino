a6d843ca8808cb3787563ee84cbad3475c866f96
MoonLordTheSecond <moonlordthesecond@abv.bg>
2025-07-17 22:43:36 +0300

    0.3.1__better_console_output
    0.3.1__better_console_output
    AND
    not finished callibration function

--- Accelerometer_with_display/Accelerometer_with_display.ino
+++ Accelerometer_with_display/Accelerometer_with_display.ino
@@ -15,15 +15,20 @@ int16_t x2, y2, z2;
 float dX, dY, dZ;
 float dXYZ;
 
-//configuration and timing virables
+//timing virables
 unsigned int Gy521_ReadsPerSecond = 10;
 unsigned int Display_FPS = 10;
-unsigned int Gy521_ReadInterval = 1000 / Gy521_ReadsPerSecond;
-unsigned int Display_WriteInterval = 1000 / Display_FPS;
+unsigned int Gy521_ReadInterval = Gy521_ReadsPerSecond/1000;
+unsigned int Display_WriteInterval = Display_FPS/1000;
 
-// converts int16 to string, resulting strings will have THE SAME LENGHT in the debug monitor. uses temporary virable
-char temp_str[3];
-char* convert_int16_to_str(int16_t i){ sprintf(temp_str, "%6d", i); return temp_str; }
+// converts int16 and float to string, resulting strings will have THE SAME LENGHT in the debug monitor. uses temporary virable
+char temp_int_str[6];//int resulting char limit
+char* convert_int16_to_str(int16_t i){ sprintf(temp_int_str, "%6d", i); return temp_int_str; }
+char temp_float_str[9];//float resulting char limit
+char* convert_float_to_str(float f) {
+    dtostrf(f, 9, 2, temp_float_str);//float resulting char limit and 2 decimal points
+    return temp_float_str;
+}
 
 
 void readFromGy521(int16_t &x, int16_t &y, int16_t &z) { //can be negative and up to +- 32000; modifies original virables, doesn't return anything
@@ -51,14 +56,9 @@ void readFromGy521(int16_t &x, int16_t &y, int16_t &z) { //can be negative and u
   GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
   GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L) */
 }
-void calcTotalDelta() {
-  //eliminate negative values, returns NaN error if not used
-  //x1 = abs(x1); y1 = abs(y1); z1 = abs(z1);
-  //x2 = abs(x1); y2 = abs(y2); z2 = abs(z2);
-  
-  dX = abs(x2-x1); dY = abs(y2-y1); dZ = abs(z2-z1);
+void calcTotalDelta() {  
+  dX = fabs(x2-x1); dY = fabs(y2-y1); dZ = fabs(z2-z1);
   dXYZ = sqrt(dX*dX + dY*dY + dZ*dZ);
-  //Serial.println(dXYZ);
 }
 
 void setup() {
@@ -68,12 +68,22 @@ void setup() {
   Wire.write(0x6B);                 // PWR_MGMT_1 - write to  Power Management register 0x6B B not 8 !
   Wire.write(0);                    // Zero - wake up MPU-6050
   Wire.endTransmission(true);       // End transmission
-  Serial.begin(9600);              // start serial communication 100 KB per second
+  Serial.begin(9600);               // start serial communication 100 KB per second
 
   /*Display setup*/
   lc.shutdown(0,false); /* Wakeup call for MAX72XX */
   lc.setIntensity(0,8); /* Set brightness to medium */
   lc.clearDisplay(0);   /* Clear display */
+
+  /* Callibration
+  float Callibration[90];
+  for(int i; i>=100; i++) {
+    readFromGy521(x1, y1, z1);
+    delay(Gy521_ReadInterval);
+    readFromGy521(x2, y2, z2);
+    calcTotalDelta();
+  }
+  */
 }
 
 enum State { WAIT_FIRST, WAIT_SECOND };
@@ -84,6 +94,7 @@ void loop() {
 
   if(currentState == WAIT_FIRST && (currentTime - prevTime >= Gy521_ReadInterval)) { 
     readFromGy521(x1, y1, z1);
+    calcTotalDelta();
 
     prevTime = currentTime;
     currentState = WAIT_SECOND;
@@ -91,22 +102,22 @@ void loop() {
 
   if(currentState == WAIT_SECOND && (currentTime - prevTime >= Gy521_ReadInterval)) { 
     readFromGy521(x2, y2, z2);
-
+    calcTotalDelta();
+    /*
+    //output to serial monitor
     Serial.print("x1: "); Serial.print(convert_int16_to_str(x1));
     Serial.print(" | y1: "); Serial.print(convert_int16_to_str(y1));
     Serial.print(" | z1: "); Serial.print(convert_int16_to_str(z1));
-
     Serial.print(" |       x2: "); Serial.print(convert_int16_to_str(x2));
     Serial.print(" | y2: "); Serial.print(convert_int16_to_str(y2));
     Serial.print(" | z2: "); Serial.print(convert_int16_to_str(z2));
-
+    */
     Serial.print(" |       dX: "); Serial.print(convert_int16_to_str(dX));
     Serial.print(" | dY: "); Serial.print(convert_int16_to_str(dY));
     Serial.print(" | dZ: "); Serial.print(convert_int16_to_str(dZ));
-    Serial.print(" |           dXYZ: "); Serial.println((dXYZ));
-
-    calcTotalDelta();
-
+    
+    Serial.print(" |           dXYZ: "); Serial.println(convert_float_to_str(dXYZ));
+    
     prevTime = currentTime;
     currentState = WAIT_FIRST;
   }
