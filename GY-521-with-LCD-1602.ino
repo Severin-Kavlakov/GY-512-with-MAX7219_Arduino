cf2a3f1bece2e6ab2ef0b799e7d99a8150bf3873
Severin Kavlakov <severinkavlakov@abv.bg>
2025-12-26 01:10:45 +0200

    0.06.1_Comments-formatting

--- GY-521-with-LCD-1602.ino
+++ GY-521-with-LCD-1602.ino
@@ -1,40 +1,42 @@
 #include <Wire.h>          //I2C library
 #include <LiquidCrystal.h> //1602 Liquid crystal library
 /* 1602 LCD pinout
-RS -     mega D7 PWM - nano D3 PWM
-Enable - mega D8 PWM - nano D5 PWM
-D4 - 	   mega D9 PWM - nano D6 PWM
-D5 - 	   mega D10 PWM SS - nano D10 PWM SS
+RS -     mega D7 PWM       - nano D3 PWM
+Enable - mega D8 PWM       - nano D5 PWM
+D4 - 	   mega D9 PWM       - nano D6 PWM
+D5 - 	   mega D10 PWM SS   - nano D10 PWM SS
 D6 - 	   mega D11 PWM MOSI - nano D11 PWM MOSI
 D7 - 	   mega D12 PWM MISO - nano D12 no-pwm MISO
 R/W - GND
 VSS - GND
 VCC - +5v */
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12); //initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12); //initialize library with the numbers of interface pins
 
 unsigned long int prevTime = millis(); //counting time in miliseconds, max is 4 294 967 295 ~~ 49,710269618055 days
 unsigned long int currentTime;
+
 const int MPU_ADDR = 0x68; //I2C address of MPU-6050.  If AD0 pin is HIGH -> I2C address = 0x69.
 bool firstRead = true;     //First or second read of GY521
 int16_t x1, y1, z1;        // first read
 int16_t x2, y2, z2;        // second read
 float dX, dY, dZ;          //Deltas of X,Y,Z
 float dXYZ;                //Sum of vectors dX+dY+dZ
+
 unsigned int Gy521_ReadsPerSecond = 50; //timing virables
 unsigned int Gy521_ReadInterval = 1000/Gy521_ReadsPerSecond; //miliseconds
 uint8_t SecondsToFindMaxdXYZ[6] = [5, 10, 15, 30, 60, 120];
 //
 uint16_t BufferArray[BufferForMaxdXYZSeconds*Gy521_ReadsPerSecond];
 
-/*Converts float to string, output has THE SAME LENGHT in SERIAL MONITOR*/
+/*Converts float to string, output has the SAME LENGHT in SERIAL MONITOR*/
 char temp_float_str[6];//output char limit
 char* convert_float_to_str(float f) {dtostrf(f, sizeof(temp_float_str), 0/*decimals*/, temp_float_str); return temp_float_str;}
 
-void readFromGy521(int16_t &x, int16_t &y, int16_t &z) { //+-32768 +-2G; modifies original virables-doesn't return anything
+void readFromGy521(int16_t &x, int16_t &y, int16_t &z) { //+-32768 +; modifies original virables
   Wire.begin();
   Wire.beginTransmission(0x68);        //Start communicating to MPU_ADDR
   Wire.write(0x3B);                    // starting register 0x3B (ACCEL_XOUT_H)
-  Wire.endTransmission(false);         // connection is kept active
+  Wire.endTransmission(false);         // keep connection active
   Wire.requestFrom(0x68, 6, true);     //Request 6 registers
   x = Wire.read()<<8 | Wire.read();    // reading: 0x3B(ACCEL_XOUT_H) and 0x3C(ACCEL_XOUT_L)
   y = Wire.read()<<8 | Wire.read();    // reading: 0x3D(ACCEL_YOUT_H) and 0x3E(ACCEL_YOUT_L)
@@ -50,12 +52,12 @@ void readFromGy521(int16_t &x, int16_t &y, int16_t &z) { //+-32768 +-2G; modifie
 }
 void calcTotalDelta() {
   dX = abs(x2-x1); dY = abs(y2-y1); dZ = abs(z2-z1);
-  dXYZ = ( sqrt(dX*dX + dY*dY + dZ*dZ) /16384)*9810; // 1G =16384parts = 9.81m/s^2 = 9810mm/s^2
+  dXYZ = ( sqrt(dX*dX + dY*dY + dZ*dZ) /16384)*9810; //1G =16384parts = 9.81m/s^2 = 9810mm/s^2
 }
 
 void setup() {
   /*Gy-521 setup*/
-  //Wire.begin();                     //Start I2C library
+  Wire.begin();                     //Start I2C library
   Wire.beginTransmission(MPU_ADDR); // Begin transmission to I2C slave GY-521
   Wire.write(0x6B);                 // PWR_MGMT_1 - write to Power Management register 0x6B
   Wire.write(0);                    // wake up MPU-6050
