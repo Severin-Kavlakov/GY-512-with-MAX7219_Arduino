35343cb329993e4eb3ae27fc4289149a86b8d1d7
MoonLordTheSecond <moonlordthesecond@abv.bg>
2025-12-29 15:09:57 +0200

    0.08.2-working-LCD

--- GY-521-with-LCD-1602.ino
+++ GY-521-with-LCD-1602.ino
@@ -12,35 +12,31 @@ VSS - GND
 VCC - +5v */
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12); //initialize library with numbers of interface pins
 
+const uint16_t Gy521_ReadsPerSecond = 25, Gy521_ReadInterval = 1000/Gy521_ReadsPerSecond;//miliseconds
+uint16_t counter = 0;
 uint32_t prevTime = millis(); //count time in miliseconds, max is 4 294 967 295 ~~ 49,710269618055 days
 uint32_t currentTime;
 
 const int MPU_ADDR = 0x68; //I2C address of MPU-6050, If pin AD0 = HIGH -> I2C address = 0x69
-bool firstRead = true;     //First/second read of GY521
-int16_t x1, y1, z1;        // first read
-int16_t x2, y2, z2;        // second read
-float dX, dY, dZ;          //Deltas of X,Y,Z
-float dXYZ;                //Sum of vectors dX+dY+dZ
-const uint16_t Gy521_ReadsPerSecond = 25;
-const uint16_t Gy521_ReadInterval = 1000/Gy521_ReadsPerSecond;//miliseconds
+bool firstRead = true;     //First, second read of GY521
+int16_t x1, y1, z1, x2, y2, z2;
+float dX, dY, dZ;
+float dXYZ;                //Deltas of X,Y,Z
+float previousdXYZ, currentdXYZ, maxdXYZ;
 
 const uint8_t potentiometerPin = A0;
 const uint8_t bufferSizeSeconds[5] = {5, 10, 15, 30, 60};
 uint8_t index = 0;
 
-uint16_t prevMaxdXYZ = 0;
-uint16_t currentMaxdXYZ = 0;
-uint16_t counter = 0;
+char maxdXYZOutput[5];
+char* convert_float_to_str(float f) {dtostrf(f, sizeof(maxdXYZOutput), 0/*decimals*/, maxdXYZOutput); return maxdXYZOutput;}
 
-/*Converts float or int to string, output always has SAME LENGHT in SERIAL MONITOR*/
-char temp_int_str[2];//output characters limit
-char* convert_uint16_to_str(uint16_t i){sprintf(temp_int_str, "%2d", i); return temp_int_str;}
-char temp_float_str[6];//output characters limit
-char* convert_float_to_str(float f) {dtostrf(f, sizeof(temp_float_str), 0/*decimals*/, temp_float_str); return temp_float_str;}
+char IndexOutput[2];
+char* convert_uint16_to_str(uint16_t i) {sprintf(IndexOutput, "%2d", i); return IndexOutput;}
 
 void readFromGy521(int16_t &x, int16_t &y, int16_t &z) { //+-32768 +; modifies original virables
   Wire.begin();
-  Wire.beginTransmission(0x68);        //Start communicating to MPU_ADDR
+  Wire.beginTransmission(MPU_ADDR);    //Start communicating to MPU_ADDR
   Wire.write(0x3B);                    // start with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);         // keep connection active
   Wire.requestFrom(0x68, 6, true);     //Request 6 registers
@@ -69,14 +65,17 @@ void setup() {
   Wire.write(0x6B);                 // write to Power Management(PWR_MGMT_1) register 0x6B
   Wire.write(0);                    // wake up MPU-6050
   Wire.endTransmission(true);       //End transmission
-  //Serial.begin(9600);               // start serial communication 9600 bits/second
-  lcd.begin(16, 2); //LCD setup - columns, rows 
+  /*LCD setup - columns, rows*/
+  lcd.begin(16, 2); 
+
+  Serial.begin(9600);               // start serial communication 9600 bits/second
 }
 void loop() {
   currentTime = millis();
+  currentdXYZ = dXYZ;
 
   /*index to choose from different buffer sizes in seconds*/
-  index = map(analogRead(potentiometerPin), 0, 1023, 0, 5); constrain(index, 0, 5);
+  index = map(analogRead(potentiometerPin), 0, 1023, 0, 5);
 
   if(firstRead && (currentTime - prevTime >= Gy521_ReadInterval)) { //Gyro FIRST read
     readFromGy521(x1, y1, z1);
@@ -89,15 +88,24 @@ void loop() {
     calcTotalDelta();
     ++counter;//one cycle of dXYZ calculation completed
 
-    //Serial.print("dXYZ: "); Serial.println(convert_float_to_str(dXYZ));               //output to serial monitor
-    lcd.setCursor(0, 0); lcd.print("Max: "); lcd.print("19620"); lcd.print("mm/s^2"); //print result
+    if(previousdXYZ > currentdXYZ){
+      maxdXYZ = previousdXYZ;
+    }
+    if(currentdXYZ > previousdXYZ){
+      maxdXYZ = currentdXYZ;
+    }
+    previousdXYZ = currentdXYZ;
+
+    Serial.print("index output: "); Serial.println(IndexOutput);               //output to serial monitor
+    lcd.setCursor(0, 0); lcd.print("Max:"); lcd.print(convert_float_to_str(maxdXYZ)); lcd.print("mm/s^2"); //print result
     lcd.setCursor(0, 1); lcd.print("Buffer: "); lcd.print(convert_uint16_to_str(bufferSizeSeconds[index])); lcd.print("s");          
 
     prevTime = currentTime;
     firstRead = true;
   }
 }
-// 3 promenlivi
-// dosegashen max
-//  segashen max
-// broyach za broy na cikli ;;; if if if
+// 3 promenlivi:
+// + dosegashen max
+// + segashen max
+// + broyach za broy na cikli ;;;
+// maksimum v zavisimost ot broyacha
